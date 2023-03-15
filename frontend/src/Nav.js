d812125fe8a94f9357305ecfea92fb35e43e2b2a
Sandeep <sandeepptpss@gmail.com>
2023-03-15 22:12:25 +0530

    update

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-const Nav = () => {
+const  Nav =()=>{
   return (
     <div >
-      <ul className='main-nav'>
+      <ul className='main-nav nav-menu'>
         <li><Link to ='/'>Home</Link></li>
         <li><Link to ='/product'>Product</Link></li>
         <li><Link to ='/add'>Add Product</Link></li>
@@ -11,9 +11,10 @@ const Nav = () => {
         <li><Link to ='/update'>Update Product</Link></li>
         <li><Link to ='/profile'>Profile</Link></li>
         <li><Link to ='/logout'>Logout</Link></li>
-      
+  
       </ul>
     </div>
   );
 }
+
 export default Nav;
