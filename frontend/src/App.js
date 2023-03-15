dd326f3e2440494830db3a66af98a0b834209935
Sandeep <sandeepptpss@gmail.com>
2023-03-15 22:11:56 +0530

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Nav from './Nav'
+import Footer from './footer';
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      
       <Nav />
       <h2>Shopping Cart Dashboard</h2>
+
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
